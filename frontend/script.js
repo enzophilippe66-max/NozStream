132a64baca738b8faa7fa185b9e7297c844f1b8a
enzophilippe66-max <enzophilippe66@gmail.com>
2026-07-29 02:19:36 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,32 +1,3 @@
-const API = "https://nozstream-2.onrender.com";
-
-
-async function register(){
-
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-
-    const response = await fetch(API + "/register", {
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify({
-            email:email,
-            password:password
-        })
-    });
-
-
-    const data = await response.json();
-
-    document.getElementById("result").innerHTML = data.message;
-
-}
-
-
-
 async function login(){
 
     const email = document.getElementById("email").value;
@@ -47,6 +18,12 @@ async function login(){
 
     const data = await response.json();
 
-    document.getElementById("result").innerHTML = data.message;
+
+    if(data.message === "Connexion réussie"){
+        window.location.href = "home.html";
+    }
+    else{
+        document.getElementById("result").innerHTML = data.message;
+    }
 
 }
